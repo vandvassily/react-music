fd36593777761254f4474024c34d7ddf0dcfd3d3
vandvassily <zhs25427199@gmail.com>
2021-09-15 01:19:36 +0800

    初始化布局

--- src/layouts/HomeLayout/index.tsx
+++ src/layouts/HomeLayout/index.tsx
@@ -1,16 +1,36 @@
 import { renderRoutes, RouteConfigComponentProps } from 'react-router-config';
 import { NavLink } from 'react-router-dom';
 
+import './index.less';
+
 const Home: React.FC<RouteConfigComponentProps> = (props) => {
   const { route } = props;
 
   return (
     <div>
       <div>This is HomeLayout</div>
-      <div className="nav-bar">
-        <NavLink to="/about">about</NavLink>
-        <NavLink to="/help">help</NavLink>
-        <NavLink to="/recommend">recommend</NavLink>
+      <div className="navbar">
+        <NavLink
+          className="navbar-item"
+          activeClassName="navbar-item-selected"
+          to="/recommend"
+        >
+          推荐页面
+        </NavLink>
+        <NavLink
+          className="navbar-item"
+          activeClassName="navbar-item-selected"
+          to="/about"
+        >
+          关于页面
+        </NavLink>
+        <NavLink
+          className="navbar-item"
+          activeClassName="navbar-item-selected"
+          to="/help"
+        >
+          帮助页面
+        </NavLink>
       </div>
       {route && renderRoutes(route.routes)}
     </div>
